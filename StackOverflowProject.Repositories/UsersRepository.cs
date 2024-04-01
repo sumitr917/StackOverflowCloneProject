39b1df8cda0ff187e42904ece34532a12cd2ab2e
Sumit Ranjan <jcsumit917@gmail.com>
2024-04-01 14:59:15 +0530

    second commit

---
+++ StackOverflowProject.Repositories/UsersRepository.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StackOverflowProject.DomainModels;
+
+namespace StackOverflowProject.Repositories
+{
+    public interface IUsersRepository
+    {
+        void InsertUser(User u);
+        void UpdateUserDetails(User u);
+        void UpdateUserPassword(User u);
+        void DeleteUser(int UserID);
+        List<User> GetUsers();
+        List<User> GetUsersByEmailAndPassword(string email, string password);
+        List<User> GetUsersByEmail(string email);
+        List<User> GetUsersByUserID(int UserID);
+        int GetLatestUserID();
+    }
+    public class UsersRepository : IUsersRepository
+    {
+        StackOverflowDatabaseDbContext db;
+        public UsersRepository()
+        {
+            db = new StackOverflowDatabaseDbContext();
+        }
+
+        public void DeleteUser(int UserID)
+        {
+            User us = db.Users.Where(temp => temp.UserID == UserID).FirstOrDefault();
+            if(us != null)
+            {
+                db.Users.Remove(us);
+                db.SaveChanges();
+            }
+        }
+
+        public int GetLatestUserID()
+        {
+            int uid = db.Users
+                .Select(temp => temp.UserID)
+                .Max();
+            return uid;
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> us = db.Users
+                .Where(temp =>
+            temp.IsAdmin == false)
+                .OrderBy(temp => temp.Name)
+                .ToList();
+            return us;
+        }
+
+        public List<User> GetUsersByEmail(string email)
+        {
+            List<User> us = db.Users.Where(temp => temp.Email.Equals(email)).ToList();
+            return us;
+        }
+
+        public List<User> GetUsersByEmailAndPassword(string email, string password)
+        {
+            List<User> us = db.Users.Where(temp => temp.Email.Equals(email) && temp.PasswordHash.Equals(password)).ToList();
+            return us;
+        }
+
+        public List<User> GetUsersByUserID(int UserID)
+        {
+            List<User> us = db.Users
+                .Where(temp => temp.UserID == UserID
+                )
+                .ToList();
+            return us;
+        }
+
+        public void InsertUser(User u)
+        {
+            db.Users.Add(u);
+            db.SaveChanges();
+        }
+
+        public void UpdateUserDetails(User u)
+        {
+            User us = db.Users.Where(temp => temp.UserID == u.UserID).FirstOrDefault();
+            if (us != null)
+            {
+                us.Name = u.Name;
+                us.Mobile = u.Mobile;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateUserPassword(User u)
+        {
+            User us = db.Users.Where(temp => temp.UserID == u.UserID).FirstOrDefault();
+            if(us != null)
+            {
+                us.PasswordHash = u.PasswordHash;
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ StackOverflowProject.ViewModels/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StackOverflowProject.ViewModels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Persistent System Ltd")]
+[assembly: AssemblyProduct("StackOverflowProject.ViewModels")]
+[assembly: AssemblyCopyright("Copyright © Persistent System Ltd 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fdcd211b-2d6f-4a43-967c-1f50d625ee13")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
